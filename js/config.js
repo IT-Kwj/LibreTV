8c47382b3df0d74bf56c7514a55cc993d0f4b7ea
IT-Kwj <49737232+IT-Kwj@users.noreply.github.com>
2025-08-05 11:13:50 +0800

    Update config.js

--- js/config.js
+++ js/config.js
@@ -114,6 +114,38 @@ const API_SITES = {
         api: 'https://cj.lziapi.com/api.php/provide/vod/',
         name: '量子资源站'
     },
+    ckzy: {
+        api: 'https://www.ckzy1.com',
+        name: 'CK资源'
+    },
+    jkun: {
+        api: 'https://jkunzyapi.com',
+        name: 'jkun资源'
+    },
+    bwzy: {
+        api: 'https://api.bwzym3u8.com',
+        name: '百万资源'
+    },
+    souav: {
+        api: 'https://api.souavzy.vip',
+        name: 'souav资源'
+    },
+    r155: {
+        api: 'https://155api.com',
+        name: '155资源'
+    },
+    lsb: {
+        api: 'https://apilsbzy1.com',
+        name: 'lsb资源'
+    },
+    huangcang: {
+        api: 'https://hsckzy.vip',
+        name: '黄色仓库'
+    },
+    yutu: {
+        api: 'https://yutuzy10.com',
+        name: '玉兔资源'
+    },
     testSource: {
         api: 'https://www.example.com/api.php/provide/vod',
         name: '空内容测试源',
